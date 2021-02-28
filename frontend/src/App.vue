16a6ed2596467d42f9ad016fd08d5151e79fcc7f
Jon Newton <newt@hey.com>
2021-02-28 14:37:50 -0500

    Setup frontend skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>{{ message }}</main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, World'
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  lintOnSave: 'error',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': __dirname + '/frontend/src'
+      }
+    },
+    entry: {
+      app: './frontend/src/main.js'
+    }
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].template = './frontend/public/index.html';
+      args[0].title = 'Holliston Real Estate Sales';
+      return args;
+    });
+  }
+};
