20455e0718c5bca843fd4d8220e44fee65ca4917
Jon Newton <newt@hey.com>
2021-02-28 23:23:24 -0500

    Data is on the glass

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <main>
-    <h1>{{ message }} <span class="badge badge-secondary">World</span></h1>
-    <div v-for="item in data" :key="item.price">
-      {{ item.address }}
+    <div class="jumbotron">
+      <h1 class="display-4">Holliston Real Estate Sales</h1>
+      <p class="lead">
+        This website provides basic real estate sales data that is easy to
+        search and available via API.
+      </p>
+
+      <hr class="my-4" />
+
+      <div class="container">
+        <div class="row">
+          <h3
+            class="col"
+            style="text-transform: capitalize"
+            v-for="label in headers"
+            :key="label"
+          >
+            {{ label }}
+          </h3>
+        </div>
+        <div class="row" v-for="item in data" :key="item.price">
+          <div class="col" v-for="label in headers" :key="label">
+            {{ item[label] }}
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -16,6 +39,7 @@ export default {
     return {
       message: 'Hello',
       count: 0,
+      headers: [],
       data: []
     };
   },
@@ -29,10 +53,15 @@ export default {
           }
         }
       );
-      console.log('response', response);
+
       this.data = response.data.data;
       this.count = response.data.count;
-      console.log('data', this.data);
+
+      if (this.count > 0) {
+        for (let name in this.data[0]) {
+          this.headers.push(name);
+        }
+      }
     } catch (error) {
       console.error('failed to call API', error);
     }
