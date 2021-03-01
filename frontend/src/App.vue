c9b254ecd8909116ff6ed48fd8100b6c3b5cb0e2
Jon Newton <newt@hey.com>
2021-02-28 21:36:11 -0500

    Switch back to vue3 and add bootstrap/jquery

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    {{ message }}
+    <h1>{{ message }} <span class="badge badge-secondary">New</span></h1>
   </main>
 </template>
 
@@ -14,14 +14,3 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
