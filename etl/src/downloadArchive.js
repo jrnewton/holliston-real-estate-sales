c620bbb70b087fb33021fd53a8c02eaf1aacdce3
Jon Newton <newt@hey.com>
2021-03-02 00:44:45 -0500

    Script to grab the 10 year archive

---
+++ etl/src/downloadArchive.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+const FormData = require('form-data');
+const Axios = require('axios');
+const $ = require('cheerio');
+
+const sleep = async (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+(async () => {
+  for (let i = 1; i <= 21; i++) {
+    console.log('page', i);
+    const formData = new FormData();
+    formData.append('page', i + '');
+    try {
+      const response = await Axios.post(
+        'http://archive.hollistonreporter.com/search?search=Recent+Real+Estate+Sales',
+        formData,
+        {
+          headers: formData.getHeaders()
+        }
+      );
+
+      const html = response.data;
+      const links = $('p.continue a', html);
+      for (const link of links) {
+        const href = link.attribs.href;
+        console.log(href);
+
+        try {
+          const listingPageResponse = await Axios.get(
+            'http://archive.hollistonreporter.com' + href,
+            {
+              responseType: 'stream'
+            }
+          );
+
+          const downloadPath = '../archive/' + href.replace(/\//g, '_');
+          listingPageResponse.data.pipe(fs.createWriteStream(downloadPath));
+          console.log('  saved', downloadPath);
+          console.log(
+            '    ',
+            `aws s3 cp ${downloadPath} s3://hres-archives/20210301/`
+          );
+        } catch (error) {
+          console.log('  error', error);
+        }
+
+        sleep(1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    sleep(5000);
+  }
+})();
